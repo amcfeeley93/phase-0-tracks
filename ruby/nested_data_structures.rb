aad3e776d0a1b4220b3adf707698dca47634bcd2
amcfeeley93 <a_mcfeeley@yahoo.com>
2016-04-04 00:33:20 -0700

    add nests

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,23 @@
+taco_supplies = {
+  tortilla: [
+    "corn",
+    "flour"
+    ],
+    tomato: [
+      "Cherry",
+      "Herloom"
+      ],
+
+  hot_sauce: [
+    "tapatio",
+    "texas pete",
+    "sriracha"
+  ],
+  meat: [
+          beef: "beef",
+      chicken: "black bean"
+    ],
+}
+
+p taco_supplies[:tortilla][:tomato][:hot_sauce][:meat]
+
