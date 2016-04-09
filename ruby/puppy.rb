a3d0aac53e0ee6d33e0b6a65e7689068c122f1c6
Autumn McFeeley <a_mcfeeley@yahoo.com>
2016-04-09 03:00:46 -0700

    add puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,26 @@
+# Species
+# puppy
+
+# Characteristics
+# Color: varies
+# Friendly: yes
+# Name: varies
+# Legs: 4 
+
+# Behavior
+# Run
+# Fetch
+# Bark 
+
+class Puppy 
+	end 
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+spot.class
+duchess == fido
+fido.instance_of?(Array)
+fido.instance_of?(Puppy)
+spot.play_dead
