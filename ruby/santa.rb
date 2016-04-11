5dc573be73f3d370719499277d78dac00c85d0a1
Autumn McFeeley <a_mcfeeley@yahoo.com>
2016-04-11 09:49:57 -0700

    add santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,11 +5,11 @@ def initialize
     @gender = gender
     @ethnicity = ethnicity
     @age = 0 
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen" ]]
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen" ]
 end 
 
 def speak 
-	print "Ho, ho, ho! Haaaaappy holdidays!"
+	print "Ho, ho, ho! Haaappy holdidays!"
 end 
 
 def eat_milk_and_cookies (cookie_type)
@@ -28,7 +28,7 @@ end
 
 santas []
 example_genders = ["agender", "female", "bigender","male", "gender fluid", "N/A"]
-example_ethnicities = ["black",  "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]]
+example_ethnicities = ["black",  "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 until santas.length == 50
   santa = Santa.new(example_genders[rand(6)], example_ethnicities[rand(7)])
   santa.age = rand(140)
@@ -39,4 +39,4 @@ puts "There are now #{santas.length} Santas."
 puts ""
 puts "Print information on a Santa. For which Santa would you like info? Enter a number between 1 and 50."
 n = gets.chomp
-p santas["#{n}".to_i]
+p santas"#{n}".to_i
